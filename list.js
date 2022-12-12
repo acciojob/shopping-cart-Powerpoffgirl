83d5e4eb5b34d76b451ea95b70d1dfdb16f96db7
Jyoti <email.jyotisingh13@gmail.com>
2022-12-12 11:46:09 +0530

    Initial Commit

--- list.js
+++ list.js
@@ -1,8 +1,10 @@
 let grandtotal = 0;
 const addData = () => {
+  console.log("add fucntion caled");
   let name = document.getElementById('item-name-input').value;
   let qty = document.getElementById('item-qty-input').value;
   let price = document.getElementById('item-price-input').value;
+  //check function is the above values are empty or not.
   let table = document.getElementById('table1');
   let newRow = table.insertRow(table.rows.length - 1);
   let namecell = newRow.insertCell(0);
